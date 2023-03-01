009552fd998f83d0d3c9535e74cdd708695154ba
pyother <piotr.dominik.sobol@gmail.com>
2023-03-01 18:27:39 +0000

    nginx server configuration

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -25,7 +24,7 @@ SECRET_KEY = 'django-insecure-+_alp($r42s!yije-v*&i(hqf#1k$y4f&+@&rx_c8bgly88z2#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sobo-love-wesele.pl", "127.0.0.1"]
 
 # Application definition
 
