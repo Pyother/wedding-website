9a24dfe0c487fb8167b2fe79249b73cdc35f8b5f
pyother <piotr.dominik.sobol@gmail.com>
2023-03-01 11:41:33 +0000

    Frontend - photo placeholder, layout changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Button } from '@mui/material';
+import { Skeleton } from '@material-ui/lab';
+
+function ImageWithPlaceholder({ src }) {
+    const [isLoading, setIsLoading] = React.useState(true);
+
+    return (
+        <div style={{ position: "relative", width: "100%", height: "100%"}}>
+            <img src={src} style={{ display: isLoading ? "none" : "block", width: "100%", height: "100%" }}onLoad={() => setIsLoading(false)} />
+            { isLoading && (
+                <Skeleton variant="rounded" width="100%" height="100%" animation="pulse" style={{ position: "absolute", top: 0, left: 0 }} />
+            )}
+        </div>
+    )
+}
 
 const Home = () => {
     return (
@@ -13,14 +27,31 @@ const Home = () => {
             
             <Grid container>
                 <Grid item xs={12} md={6} className="content-left">
-                    <p></p>
+                    <ImageWithPlaceholder src="photo.png" />
                 </Grid>
                 <Grid item xs={12} md={6} className="content-right">
-                    <h2>Header </h2>
+                    <h2 style={{justifyContent: "left", alignItems: "left", padding: "0", paddingLeft: "5%"}}>Nagłówek </h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    
+                    <Grid container className="button-container">
+                        <Button style={{backgroundColor: "white", color: "#023322"}}>Kolejny element -> </Button >
+                    </Grid >
                 </Grid>
             </Grid >
+            <Grid container className="info-container">
+                <Grid item xs={12} md={8}>
+                    <h2>Informacje o Weselu</h2>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <h2>Jak do nas dotrzeć?</h2>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <h2>Księga Gości</h2>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <h2>Kontakt</h2>
+                </Grid>
+            </Grid>
+            
 
             <Container className="footer">
 
