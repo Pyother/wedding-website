6132c5fe41ac1721fd491f7bce4343a0bb534d9a
pyother <piotr.dominik.sobol@gmail.com>
2023-05-12 21:30:22 +0000

    add accomodation section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { useState, useEffect, useRef } from 'react';
-import { Container, Grid, Divider } from '@mui/material';
+import { Container, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button } from '@mui/material';
 import { Skeleton } from '@material-ui/lab';
-import Countdown from 'react-countdown';
 import background from './images/background.png';
 import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
 import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
@@ -58,7 +57,7 @@ function Counter() {
     });
 
     return (
-        <div style={{justifyContent: "center", alignItems: "center", padding: "3%"}}>
+        <div style={{justifyContent: "center", alignItems: "center", padding: "1%", border: "1px solid"}}>
             <Grid container className="counter">
                 <Grid item xs={3} md={3} className="counter-cell">
                     <p style={{fontWeight: "bold"}}><h1>{timerDays}</h1></p>
@@ -83,9 +82,9 @@ function Counter() {
 
 function FadeOutSubtitle() {
     const [padding, setPadding] = useState(5);
-  
-    useEffect(() => {
-      let targetPadding = 1;
+    
+    /*useEffect(() => {
+      let targetPadding = 2;
   
       const handleScroll = () => {
         const currentScrollPos = window.pageYOffset;
@@ -97,17 +96,61 @@ function FadeOutSubtitle() {
       
       window.addEventListener("scroll", handleScroll);
       return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+    }, []);*/
   
     return (
-      <div className="header" style={{position: "sticky", zIndex: "1", top: "0", backgroundColor: "white", paddingTop: `${padding}%`, paddingBottom: `${padding}%`}}>
+      <div className="header" style={{position: "sticky", zIndex: "1", top: "-5em", backgroundColor: "white", paddingTop: `${padding}%`, paddingBottom: `${padding}%`}}>
         <Container>
-            <h1>Kasia & Damian</h1>
-            <h2 style={{padding: "0", margin: "0"}}>Serdecznie zapraszamy na Nasze Wesele!</h2>
+            <h1 style={{marginBottom: "0.4em"}}>Kasia & Damian</h1>
+            <Grid container>
+                <Grid item xs={4} md={4} style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <Button style={{height: "5vh"}} href="#plan">
+                        <p style={{textTransform: "none", fontFamily: 'Great Vibes', color: 'black'}}>Harmonogram</p>
+                    </Button>
+                </Grid>
+                <Grid item xs={4} md={4} style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <Button style={{height: "5vh"}} href="#location">
+                        <p style={{textTransform: "none", fontFamily: 'Great Vibes', color: 'black'}}>Lokalizacja</p>
+                    </Button>
+                </Grid>
+                <Grid item xs={4} md={4} style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <Button style={{height: "5vh"}} href="#accomodation">
+                        <p style={{textTransform: "none", fontFamily: 'Great Vibes', color: 'black'}}>Zakwaterowanie</p>
+                    </Button>
+                </Grid>
+            </Grid>
         </Container>
       </div>
     );
 }
+
+function createData(hour, event) {
+    return { hour, event };
+}
+
+const rows = [
+    createData('14:00', 'Ceremonia ślubna'),
+    createData('14:50', 'Pamiątkowe zdjęcia pod kościołem'),
+    createData('15:15', 'Przybycie Pary Młodej na salę weselną'),
+    createData('16:00', 'Uroczysty obiad'),
+    createData('17:00', 'Pierwszy taniec i rozpoczęcie zabawy'),
+    createData('19:00', 'Pierwsza kolacja'),
+    createData('20:30', 'Tort'),
+    createData('21:30', 'Podziękowania dla rodziców'),
+    createData('23:00', 'Druga kolacja'),
+    createData('00:00', 'Ceremonia chaosu'),
+    createData('02:00', 'Trzecia kolacja')
+];
+
+function createData2(day, hour, event) {
+    return { day, hour, event };
+}
+
+const rows2 = [
+    createData2('sobota', '14:00', 'Od tej godziny można się meldować w hotelu'),
+    createData2('niedziela', '08:00-10:00', 'Śniadanie'),
+    createData2('niedziela', '11:00', 'Wymeldowanie')
+]
 
 const Home = () => {
 
@@ -132,80 +175,68 @@ const Home = () => {
             <Grid container className="info-container" style={{paddingBottom: "5%", backgroundColor: "white", color: "black"}}>
                 <Grid item xs={12} md={8}>
                     <h2>Informacje o Weselu</h2>
-                    <Grid container>
-                        <Grid item xs={12} md={4} className="counter-cell">
-                            <CalendarMonthOutlinedIcon className="icon"/>
-                            <p>13.05.2023, godz.14:00</p>
-                        </Grid>
-                        <Grid item xs={12} md={4} className="counter-cell">
-                            <PlaceOutlinedIcon className="icon"/>
-                            <p>Parafia pw. Św. Franciszka Salezego i św. Andrzeja Boboli w Gorzycach</p>
-                        </Grid>
-                        <Grid item xs={12} md={4} className="counter-cell">
-                            <HotelOutlinedIcon className="icon"/>
-                            <p>(Miejsce na Informacje o zakwaterowaniu)</p>
+                    <Grid item xs={12} md={12} className="counter-cell">
+                        <CalendarMonthOutlinedIcon className="icon"/>
+                        <p>13.05.2023, godz.14:00</p>
+                    </Grid>
+                    <Grid item xs={12} md={12} className="counter-cell" id="accomodation">
+                        <PlaceOutlinedIcon className="icon"/>
+                        <p>Parafia pw. Św. Franciszka Salezego i św. Andrzeja Boboli w Gorzycach</p>
+                    </Grid>
+                    <Grid item xs={12} md={12} className="counter-cell">
+                        <HotelOutlinedIcon className="icon"/>
+                        <p>Zapraszamy na portiernię po odbiór kluczy oraz informację o przydzielonym pokoju.</p>
+                        <Grid container style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                            <Grid item xs={10} md={10}>
+                                <TableContainer>
+                                    <Table>
+                                        <TableHead>
+                                            <TableCell style={{fontWeight: "bold", fontFamily: 'EB Garamond'}}>Dzień</TableCell>
+                                            <TableCell style={{fontWeight: "bold", fontFamily: 'EB Garamond'}}>Godzina</TableCell>
+                                            <TableCell style={{fontWeight: "bold", fontFamily: 'EB Garamond'}}>Wydarzenie</TableCell>
+                                        </TableHead>
+                                        <TableBody>
+                                            {rows2.map((row) => (
+                                                <TableRow key={row.day}>
+                                                    <TableCell style={{fontFamily: 'EB Garamond'}}>{row.day}</TableCell>
+                                                    <TableCell style={{fontFamily: 'EB Garamond'}}>{row.hour}</TableCell>
+                                                    <TableCell style={{fontFamily: 'EB Garamond'}}>{row.event}</TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
-                <Grid item xs={12} md={8}>
+                <Grid item xs={10} md={8} id="plan">
                     <h2>Szczegółowy plan Wesela</h2>
-                    <Grid container>
-                        <Grid item xs={3} md={3} style={{padding: "0%"}}>
-                            <p>14:00</p>
-                        </Grid>
-                        <Grid item xs={9} md={9} style={{padding: "0%"}}>
-                            <p style={{padding: "0%"}}>Ceremonia ślubna</p>
-                        </Grid>
-                    </Grid>
-                    <Grid container>
-                        <Grid item xs={3} md={3} style={{padding: "0%"}}>
-                            <p>14:50</p>
-                        </Grid>
-                        <Grid item xs={9} md={9} style={{padding: "0%", textAlign: "left"}}>
-                            <p style={{padding: "0%"}}>Pamiątkowe zdjęcia przed kościołem</p>
-                        </Grid>
-                    </Grid>
-                    <Grid container>
-                        <Grid item xs={3} md={3} style={{padding: "0%"}}>
-                            <p>15:15</p>
-                        </Grid>
-                        <Grid item xs={9} md={9} style={{padding: "0%"}}>
-                            <p style={{padding: "0%"}}>Przybycie pary młodej na salę weselną</p>
-                        </Grid>
-                    </Grid>
-                    <Grid container>
-                        <Grid item xs={3} md={3} style={{padding: "0%"}}>
-                            <p>16:00</p>
-                        </Grid>
-                        <Grid item xs={9} md={9} style={{padding: "0%", textAlign: "left"}}>
-                            <p style={{padding: "0%"}}>Uroczysty obiad</p>
-                        </Grid>
-                    </Grid>
-                    <Grid container>
-                        <Grid item xs={3} md={3} style={{padding: "0%"}}>
-                            <p>17:00</p>
-                        </Grid>
-                        <Grid item xs={9} md={9} style={{padding: "0%"}}>
-                            <p style={{padding: "0%"}}>Pierwszy taniec i rozpoczęcie zabawy</p>
-                        </Grid>
-                    </Grid>
-                    <Grid container>
-                        <Grid item xs={3} md={3} style={{padding: "0%"}}>
-                            <p>19:00</p>
-                        </Grid>
-                        <Grid item xs={9} md={9} style={{padding: "0%", textAlign: "left"}}>
-                            <p style={{padding: "0%"}}>Pierwsza kolacja</p>
-                        </Grid>
-                    </Grid>
+                    <TableContainer>
+                        <Table>
+                            <TableHead>
+                                <TableCell style={{fontWeight: "bold", fontFamily: 'EB Garamond'}}>Godzina</TableCell>
+                                <TableCell style={{fontWeight: "bold", fontFamily: 'EB Garamond'}}>Wydarzenie</TableCell>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                <TableRow key={row.hour}>
+                                    <TableCell style={{fontFamily: 'EB Garamond'}}>{row.hour}</TableCell>
+                                    <TableCell style={{fontFamily: 'EB Garamond'}}>{row.event}</TableCell>
+                                </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </Grid>
-                <Grid item xs={12} md={8}>
+                <Grid item xs={12} md={8} id="location">
                     <h2>Jak do nas dotrzeć?</h2> 
                     <iframe data-testid="iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4794.657451332664!2d21.781786569486854!3d50.73779075078343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4722b670fcc66eeb%3A0x23a4ce0291aa1b21!2sDw%C3%B3r%20Dwikozy%20Hotel%20Sandomierz!5e1!3m2!1spl!2spl!4v1677711265730!5m2!1spl!2spl" style={{width: "100%", height: "50vh", border: "0"}} loading="lazy"></iframe>                   
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <h2>Księga Gości</h2>
                 </Grid>
-                <Grid item xs={12} md={8}>
+                <Grid item xs={12} md={8} id="contact">
                     <h2>Kontakt</h2>
                     <Grid container>
                         <Grid item xs={6} md={6}>
